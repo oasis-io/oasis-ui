51c94a316be37f33546d6f4abe894e4d2b722f91
zhangshaodong <shaodong2022@gmail.com>
2023-06-08 15:59:26 +0800

    Change Home color and style

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,61 @@
 <template>
-    <div class="container">
-        <el-card class="box-card" style="height: 100px; margin-bottom: 20px; background-color: #E0E7FF;">
-            <div class="text-center" slot="header">
-                <h2>Welcome to Oasis</h2>
-            </div>
-        </el-card>
-        <el-card class="box-card" style="height: 300px; margin-bottom: 20px; background-color: #F0F4C3;">
-            <div slot="header">
-                <span>Card 2</span>
-            </div>
-        </el-card>
-        <div class="bottom-cards">
-            <el-card class="box-card" style="height: 400px; margin-right: 10px; background-color: #D1C4E9;">
-                <div slot="header">
-                    <span>Card 3</span>
+    <div class="dashboard-container">
+        <div class="header-card">
+            <el-card class="content-card" style="height: 100px; background-color: #34495E;">
+                <div class="text-space-between" slot="header">
+                    <h2 style="color: #fff;">Welcome to Oasis</h2>
+                    <div class="help-and-docs">
+                        <div class="help-link">
+                            <h3 style="color: #fff;">Help?</h3>
+                        </div>
+                        <div class="docs-link">
+                            <h5><a href="https://github.com/oasis-io/oasis-ui" target="_blank"
+                                    style="color: #fff;">Documentation</a></h5>
+                        </div>
+                    </div>
                 </div>
             </el-card>
-            <el-card class="box-card" style="height: 400px; background-color: #FFE0B2;">
+        </div>
+        <div class="status-cards">
+            <el-card class="system-status-card" style="background-color: #0c5460;">
                 <div slot="header">
-                    <span>Card 4</span>
+                    <span>系统状态：OK</span>
+                </div>
+            </el-card>
+            <div class="additional-status-cards">
+                <div class="status-card-pair">
+                    <el-card class="status-card" style="background-color: #0d6674;">
+                        <div slot="header">
+                            <span>运行时间：245d</span>
+                        </div>
+                    </el-card>
+                    <el-card class="status-card" style="background-color: #138086;">
+                        <div slot="header">
+                            <span>用户数：8</span>
+                        </div>
+                    </el-card>
+                </div>
+                <div class="status-card-pair">
+                    <el-card class="status-card" style="background-color: #379392;">
+                        <div slot="header">
+                            <span>实例数：264</span>
+                        </div>
+                    </el-card>
+                    <el-card class="status-card" style="background-color: #5ca3a9;">
+                        <div slot="header">
+                            <span>正在登陆：2</span>
+                        </div>
+                    </el-card>
+                </div>
+            </div>
+        </div>
+        <div class="announcement-section">
+            <el-card class="content-card" style="height: 300px;">
+                <div slot="header">
+                    <h2>通知</h2>
+                </div>
+                <div>
+                    <p>这里是你的通知内容。</p>
                 </div>
             </el-card>
         </div>
@@ -30,24 +67,59 @@ import { ElCard } from 'element-plus';
 </script>
 
 <style scoped>
-.container {
+.dashboard-container {
     display: flex;
     flex-direction: column;
     align-items: stretch;
+    gap: 20px;
 }
 
-.bottom-cards {
+.text-space-between {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+}
+
+.help-and-docs {
+    display: flex;
+    align-items: center;
+}
+
+.help-link {
+    margin-right: 20px;
+}
+
+.content-card {
+    width: 100%;
+}
+
+.status-cards {
     display: flex;
     justify-content: space-between;
 }
 
-.box-card {
-    width: 100%;
+.system-status-card {
+    width: 40%;
+    height: 300px;
+    margin-right: 20px;
 }
 
-.text-center {
+.additional-status-cards {
     display: flex;
-    align-items: center;
-    justify-content: flex-start;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 58%;
+}
+
+.status-card-pair {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+}
+
+.status-card {
+    width: 48%;
+    height: 140px;
 }
 </style>
